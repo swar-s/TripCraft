3f291f411aabdbbd9b21ecc621db5244c7eafd3d
Swarup Suryawanshi <94120592+swar-s@users.noreply.github.com>
2024-10-16 21:35:50 +0530

    Negligible

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import { Button } from './components/ui/button'
+// import { Button } from './components/ui/button'
 import Hero from './components/custom/Hero'
 
 function App() {
